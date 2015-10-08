96268b1d3077143d035a72a9707458fa776f863d
Felipe Gutierrez <fgutierrez3@wisc.edu>
2015-10-08 16:29:07 -0500

    Updated slurm_charm_setup to append CHARMDIR to user's .bashrc

--- slurm_charm_setup.sh
+++ slurm_charm_setup.sh
@@ -2,6 +2,7 @@
 
 #SBATCH -N 1 -n 4 -t 0-00:15:00
 #SBATCH -o charm-build-log.o%j
+#SBATCH -p slurm_me759
 
 cd $SLURM_SUBMIT_DIR/..
 
@@ -10,4 +11,8 @@ tar -zxvf charm-6.6.1.tar.gz
 cd charm
 
 ./build charm++ multicore-linux64 gcc gfortran --with-production --enable-tracing -j4
+
+echo \
+export CHARMDIR=$SLURM_SUBMIT_DIR/../charm \
+ >> $HOME/.bashrc
 
