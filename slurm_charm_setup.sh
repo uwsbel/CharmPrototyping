21f479e4566b1680174abcaa9e8a477d47d61761
Colin Vanden Heuvel <thepianoboy@users.noreply.github.com>
2015-10-08 16:13:53 -0500

    Create slurm_charm_setup.sh
    Adds a charm build script for use with the Euler Cluster.

---
+++ slurm_charm_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -N 1 -n 4 -t 0-00:15:00
+#SBATCH -o charm-build-log.o%j
+
+cd $SLURM_SUBMIT_DIR/..
+
+wget http://charm.cs.illinois.edu/distrib/charm-6.6.1.tar.gz
+tar -zxvf charm-6.6.1.tar.gz
+cd charm
+
+./build charm++ multicore-linux64 gcc gfortran --with-production --enable-tracing -j4
+
