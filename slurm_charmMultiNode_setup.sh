f2016dace6b5d4b5ab2b290047cb70ec60c4ac7b
Felipe Gutierrez <fgutierrez3@wisc.edu>
2016-03-10 16:01:42 -0600

    Update slurm_charmMultiNode_setup.sh

--- slurm_charmMultiNode_setup.sh
+++ slurm_charmMultiNode_setup.sh
@@ -9,7 +9,7 @@ wget http://charm.cs.illinois.edu/distrib/charm-6.6.1.tar.gz
 tar -zxvf charm-6.6.1.tar.gz
 cd charm
 
-./build charm++ mpi-linux-x86_64 mpicxx smp  gfortran  -j4  --with-production --enable-tracing
+./build charm++ netlrts-linux-x86_64 g++ gfortran -j4  --with-production --enable-tracing
 
 echo \
 export CHARMDIR=$SLURM_SUBMIT_DIR/../charm \
